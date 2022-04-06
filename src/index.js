e1d3e65852cdad561d06ed32b54bcb8a06c57f9d
Shrivastava <reyansh@5c52309a0a64.ant.amazon.com>
2022-04-06 11:31:15 +0530

    added react app

--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
-import React from "react";
-import { render } from "react-dom";
-import { createGlobalStyle } from "styled-components";
+import React from 'react';
+import ReactDom from 'react-dom';
 
-import Root from "./components/Root";
+import App from './App';
 
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
-
-  html, body {
-    margin: 0;
-    padding: 0;
-  }
-
-  html, body, #app {
-    height: 100%;
-    width: 100%;
-  }
-
-  body {
-    font-family: Montserrat, sans-serif;
-  }
-`;
-
-render(
-  <>
-    <GlobalStyle />
-    <Root />
-  </>,
-  document.getElementById("app")
-);
+ReactDom.render(<App />, document.getElementById('root'));
